0f3479b1235845616db4f926df80fb9059b94676
keerthivasan05 <72241111+keerthivasan05@users.noreply.github.com>
2020-10-16 10:27:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/scr/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home:Home(),
+    );
+  }
+}
---
+++ lib/scr/commons.dart
@@ -0,0 +1,6 @@
+import'package:flutter/material.dart';
+
+const Color red=Colors.red;
+const Color white=Colors.white;
+const Color grey=Colors.grey;
+const Color black=Colors.black;
